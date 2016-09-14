279815c0a3aa33d8aab77c90d6ed21955b84a64c
techielit <fourwaldens@gmail.com>
2016-09-14 16:11:14 -0400

    test

--- Exercise 1/test4.py
+++ Exercise 1/test4.py
@@ -1,5 +1,10 @@
 def find_first_2_letter_word(xs):
     for wd in xs:
         if len(wd) == 2:
-            return wd
+            print(wd)
+            return ""
     return ""
+
+find_first_2_letter_word(["and", "how", "do", "you", "do"])
+print()
+print()
